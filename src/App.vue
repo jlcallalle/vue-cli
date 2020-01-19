3457bf0406520dd1221b63585837f95cb1d36271
Jorge <jlcallalle@gmail.com>
2020-01-19 17:22:19 -0500

    start proyect vue-cli

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="bg-blue-400">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- Muestra componente creado-->
+    <HelloWorld msg="Welcome to Your Vue.js App 123" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue"; //es5
+
+export default {
+  //es5
+  name: "app",
+  components: {
+    //vue.componentes
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
