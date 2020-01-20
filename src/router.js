b216e281a838f44177354b38caa3bd1b84f28b27
Jorge <jlcallalle@gmail.com>
2020-01-19 20:30:08 -0500

    router vue

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
-
+import About from '@/views/About'
+import Error from '@/views/Error'
 
 Vue.use(Router)
 
@@ -13,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '*',
+      name: 'Error',
+      component: Error
     }
   ]
 })
